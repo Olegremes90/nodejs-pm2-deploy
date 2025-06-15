8362fa3dcde981451074bd0fbb89823ae55508f8
Olegremes90 <113620810+Olegremes90@users.noreply.github.com>
2025-06-15 15:46:13 +0300

    подправил файл конфига2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,29 @@
 require('dotenv').config({ path: '.env.deploy' });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main' } = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master' } = process.env;
 
 module.exports = {
+  apps: [
+    {
+      name: 'frontend',
+      script: 'npx',
+      args: ['serve', '-s', 'build', '-l', '3001'], // раздача статических файлов из build на порту 3000
+      cwd: '/home/mesto/my-app/source/frontend',                    // директория проекта на сервере (где есть папка build)
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+      },
+      watch: false
+    },
+    {
+      name: 'backend',
+      script: 'dist/app.js',
+      cwd: '/home/mesto/source/backend',
+      env: { NODE_ENV: 'production' },
+      watch: false,
+    }
+  ],
+
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -11,12 +32,7 @@ module.exports = {
       repo: 'git@github.com:Olegremes90/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/.env`,
-      'post-deploy': `
-        cd frontend && npm install && npm run build &&
-        cd ../backend && npm install &&
-        pm2 restart ecosystem.front.config.js --only frontend &&
-        pm2 restart ecosystem.backend.config.js --only backend
-      `,
+      'post-deploy': 'cd frontend && export NODE_OPTIONS=--openssl-legacy-provider && source ~/.nvm/nvm.sh && npm install && npm run build && pm2 startOrReload ecosystem.config.js --only frontend',
       ssh_options: 'StrictHostKeyChecking=no'
     }
   }
